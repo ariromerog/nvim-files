25a44fdea7607f69a196c896df0311fda9887263
Ari Romero <ari.romero-contractor@procore.com>
2023-02-09 10:48:45 -0300

    fix diagnostics

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,3 +10,11 @@ lsp.ensure_installed({
 	'sumneko_lua'
 })
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
