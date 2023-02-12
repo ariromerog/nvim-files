a0684c08f3fdd67e916c58f05d74d065b2f6e8c7
Ari Romero <ari.romero@gmail.com>
2023-02-12 12:08:13 -0300

    use onedark warm

--- lua/ari/init.lua
+++ lua/ari/init.lua
@@ -5,12 +5,10 @@ require("ari.null-ls")
 require("ari.prettier")
 
 -- Color Scheme
-local colorscheme = "nordfox"
-
-local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
-if not status_ok then
-  return
-end
+require('onedark').setup {
+  style = 'warm'
+}
+require('onedark').load()
 
 -- autocmds
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
