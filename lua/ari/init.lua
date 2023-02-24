731da0d5e78768a629836e05e78d1b7d9f7a3a64
Ari Romero <ari.romero@gmail.com>
2023-02-23 23:30:50 -0300

    added autopairs

--- lua/ari/init.lua
+++ lua/ari/init.lua
@@ -3,6 +3,7 @@ require("ari.packer")
 require("ari.settings")
 require("ari.null-ls")
 require("ari.prettier")
+require("ari.autopairs")
 
 -- Color Scheme
 require('onedark').setup {
--- lua/ari/packer.lua
+++ lua/ari/packer.lua
@@ -1,56 +1,53 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-      use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
-      use {
-          'nvim-telescope/telescope.nvim', tag = '0.1.0',
-          requires = { { 'nvim-lua/plenary.nvim' } }
-      }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-      use { 'navarasu/onedark.nvim' }
+  use { 'navarasu/onedark.nvim' }
 
-      use({
-          'rose-pine/neovim',
-          as = 'rose-pine'
-      })
+  use 'EdenEast/nightfox.nvim'
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use 'theprimeagen/harpoon'
+  use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
+  use 'windwp/nvim-autopairs'
 
-      use 'EdenEast/nightfox.nvim'
-      use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-      use 'theprimeagen/harpoon'
-      use 'mbbill/undotree'
-      use 'tpope/vim-fugitive'
 
-      use {
-          'nvim-tree/nvim-tree.lua',
-          requires = {
-              'nvim-tree/nvim-web-devicons', -- optional, for file icons
-          }
-      }
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    }
+  }
 
-      use {
-          'VonHeikemen/lsp-zero.nvim',
-          requires = {
-              -- LSP Support
-              { 'neovim/nvim-lspconfig' },
-              { 'williamboman/mason.nvim' },
-              { 'williamboman/mason-lspconfig.nvim' },
-              { 'jose-elias-alvarez/null-ls.nvim' },
-              { 'MunifTanjim/prettier.nvim' },
-              -- Autocompletion
-              { 'hrsh7th/nvim-cmp' },
-              { 'hrsh7th/cmp-buffer' },
-              { 'hrsh7th/cmp-path' },
-              { 'saadparwaiz1/cmp_luasnip' },
-              { 'hrsh7th/cmp-nvim-lsp' },
-              { 'hrsh7th/cmp-nvim-lua' },
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'jose-elias-alvarez/null-ls.nvim' },
+      { 'MunifTanjim/prettier.nvim' },
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
-              -- Snippets
-              { 'L3MON4D3/LuaSnip' },
-              { 'rafamadriz/friendly-snippets' },
-          }
-      }
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  }
 
-      use 'nvim-tree/nvim-web-devicons'
-      use { 'romgrk/barbar.nvim', wants = 'nvim-web-devicons' }
-    end)
+  use 'nvim-tree/nvim-web-devicons'
+  use { 'romgrk/barbar.nvim', wants = 'nvim-web-devicons' }
+end)
